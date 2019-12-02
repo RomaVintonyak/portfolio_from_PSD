75c7ac7a7af338bab55191d2f2266271b442f140
RomaVintonyak <19roman23@gmail.com>
2019-12-02 12:19:06 +0200

    origin master

--- js/main.js
+++ js/main.js
@@ -19,4 +19,22 @@ $(function() {
       return false;
     });
   });
+  /*filter for category with works*/
+  var filter = $("[data-filter]");
+  filter.on("click", function(event) {
+    event.preventDefault();
+    var cat = $(this).data("filter");
+    if (cat == "all") {
+      $("[data-cat]").removeClass("hide");
+    } else {
+      $("[data-cat]").each(function() {
+        var workCat = $(this).data("cat");
+        if (workCat != cat) {
+          $(this).addClass("hide");
+        } else {
+          $(this).removeClass("hide");
+        }
+      });
+    }
+  });
 });
